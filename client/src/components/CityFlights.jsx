65925a359037ebcdf0bf5f36afbe3a07372979ca
bashyerOmar <besho.om.alghamdi@gmail.com>
2022-09-07 23:34:33 +0300

    add spinner

--- client/src/components/CityFlights.jsx
+++ client/src/components/CityFlights.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react"
 import axios from 'axios';
 import { useHistory, useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 const CityFlights = props => {
 
     const { city } = useParams();
     const [flights, setFlights] = useState([]);
     const history = useHistory();
+    const [loaded , setLoaded] = useState(false);
 
 
     const instance = axios.create({
@@ -17,9 +19,18 @@ const CityFlights = props => {
     })
 
     useEffect(() => {
-        instance.get(`/flights/${city}`)
-            .then(res => setFlights(res.data.flights))
+
+        setTimeout(() => {
+            instance.get(`/flights/${city.toLowerCase()}`)
+            .then(res => {
+                setFlights(res.data.flights)
+                setLoaded(true)
+            })
             .catch(err => console.log(err))
+
+
+          }, "2000")
+        
         // instance.get(`/users/${localStorage.getItem('user_email').slice(1,localStorage.getItem('user_email').length-1)}`)
         // .then(res => setUser(res.data.user))
         // .catch(err => console.log(err))
@@ -30,13 +41,14 @@ const CityFlights = props => {
         <>
             <div><h1>flights of {city}</h1></div>
             <div style={{ marginTop: "30px" }}>
-
-                {flights.map((flight) =>
+            
+                {loaded ? flights.map((flight) =>
                     <h3 key={flight._id}> {flight.source} |  {flight.destination} | {flight.price} &nbsp;
                         <Button onClick={e => history.push(`/book/${flight._id}`)} variant="warning">Book </Button> <hr /> </h3>
-                )}
-                {/* if no flights available */}
+                ) : <Spinner animation="border" />}
+             
                 {!flights.length && <h3>No flights available</h3>}
+           
             </div>
 
         </>
